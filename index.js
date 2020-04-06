05c8f75f640cf7419e8f1d1f4455bf46898bd54f
Mohammad Shoaib <xenon.9210@gmail.com>
2020-04-06 12:40:02 +0530

    Added the logging and datetime on connection

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
+const winston = require('winston');
 const express = require("express");
 const app = express();
 const net = require("net");
 const server = net.createServer();
 
+require("./logging")();
 const clients = [];
 
 app.get("/api/clients", (req, res) => {
   if (clients.length === 0) return res.send("No connections found.");
 
-  for (let key of clients) key.write(req.query.data);
+  if (req.query.data)
+    for (let key of clients) key.write(req.query.data);
 
   const sockets = [];
-  for (let client of clients) {
+  for (let key of clients) {
     sockets.push({
-      remoteAddress: client.remoteAddress,
-      remotePort: client.remotePort
+      remoteAddress: key.remoteAddress,
+      remotePort: key.remotePort,
+      dateTime: key.dateTime
     });
   }
 
@@ -22,8 +26,14 @@ app.get("/api/clients", (req, res) => {
 });
 
 server.on("connection", client => {
-  console.log(`CONNECTED: ${client.remoteAddress} : ${client.remotePort}`);
+  winston.info(`CONNECTED: ${client.remoteAddress} : ${client.remotePort}`);
   // socket.pipe(socket);
+
+  const dateTime = new Date();
+  dateTime.setHours(dateTime.getHours() + 5);
+  dateTime.setMinutes(dateTime.getMinutes() + 30);
+  client.dateTime = dateTime;
+
   clients.push(client);
 
   client.on("data", data => {
@@ -41,7 +51,7 @@ server.on("connection", client => {
 
     if (index !== -1) clients.splice(index, 1);
 
-    console.log(`CLOSED: ${client.remoteAddress} : ${client.remotePort}`);
+    winston.info(`CLOSED: ${client.remoteAddress} : ${client.remotePort}`);
   });
 
   client.on("error", () => {
@@ -49,18 +59,20 @@ server.on("connection", client => {
 
     if (index !== -1) clients.splice(index, 1);
 
-    console.log(`CLOSED: ${client.remoteAddress} : ${client.remotePort}`);
+    winston.info(`CLOSED: ${client.remoteAddress} : ${client.remotePort}`);
   });
 });
 
 // const port = process.env.PORT || 1337;
 const portTcp = 1337;
 const portHttp = 8080;
+// const host = '159.65.150.41';
+// const host = '0.0.0.0';
 
 server.listen(portTcp, () => {
-  console.log(`TCP Server is running on port ${portTcp}`);
+  winston.info(`TCP Server is running on port ${portTcp}`);
 });
 
 app.listen(portHttp, () => {
-  console.log(`Listening on port ${portHttp}`);
+  winston.info(`Listening on port ${portHttp}`);
 });
